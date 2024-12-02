d7dce4af5bb4494dfa86e839806c801c8a664680
Gess1t <tintamare91@gmail.com>
2024-12-03 00:42:31 +0100

    Fallback to OTD default notification if not connected to UX remote

--- src/IPC/Remote.cs
+++ src/IPC/Remote.cs
@@ -1,7 +1,9 @@
 using OpenTabletDriver.Desktop;
 using OpenTabletDriver.Desktop.Contracts;
 using OpenTabletDriver.External.Common.RPC;
+using OpenTabletDriver.Plugin;
 using OTD.UX.Remote.Lib;
+using System;
 using System.Threading.Tasks;
 
 namespace OTD.PresetBinds.IPC
@@ -9,18 +11,29 @@ namespace OTD.PresetBinds.IPC
     public class Remote
     {
         public static bool Connected { get; set; } = false;
-        
+
         public static RpcClient<IDriverDaemon> Driver { get; } = new("OpenTabletDriver.Daemon");
         public static RpcClient<IUXRemote> UX { get; } = new("OTD.UX.Remote");
 
         public static async Task ApplySettingsAsync(Settings settings, string name)
         {
-            await Driver.Instance.SetSettings(settings);
-
-            if (UX.IsConnected && UX.Instance != null)
+            try
             {
-                await UX.Instance.Synchronize();
-                await UX.Instance.SendNotification("Preset Binding", $"Switched to '{name}' preset");
+                await Driver.Instance.SetSettings(settings);
+
+                Log.Debug("OTD Presets", $"Switched to '{name}' preset");
+
+                if (UX.IsConnected && UX.Instance != null)
+                {
+                    await UX.Instance.Synchronize();
+                    await UX.Instance.SendNotification("Preset Binding", $"Switched to '{name}' preset");
+                }
+                else
+                    Log.Write("OTD Presets", $"Switched to '{name}' preset", LogLevel.Info, false, true);
+            }
+            catch (Exception e)
+            {
+                Log.Write("OTD Presets", $"Error: {e}", LogLevel.Error);
             }
         }
     }
